1992b4783a0c010eec2960fefc298ee18e5b8ce4
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-09-21 21:58:55 +0600

    feat: basic server

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 )
 
 type Product struct {
@@ -48,4 +50,11 @@ func main() {
 
 	fmt.Printf("connected to the database")
 	panicOnErr(db.AutoMigrate(&Product{}), "failed to migrate")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
